8bfe24d2d7e4e8ea0902c067da8336f65ae4d69f
eyader-2121 <eyaderaddisbelete@gmail.com>
2026-01-31 04:52:28 +0400

    banner building

--- netflix-2026/src/pages/Home/Home.js
+++ netflix-2026/src/pages/Home/Home.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
+import Banner from '../../components/banner/Banner';
 
 const Home = () => {
   return (
     <div>
       <Header/>
+      <Banner/>
       <Footer/>
+      
     </div>
   )
 }
--- netflix-2026/src/utils/requests.js
+++ netflix-2026/src/utils/requests.js
@@ -1,9 +1,9 @@
 const API_KEY = process.env.REACT_APP_API_KEY;
 // to hiding the API key and make it more secure and it can not be tracked in the version control system
 const requests = {
-  fetchTrending: `/trending/all/week?api_key=${API_KEY} &language=en-US`,
-  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY} &with_networks=213`,
-  fetchTopRated: `/movie/top_rated?api_key=${API_KEY} &language=en-US`,
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
   fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
   fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
   fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
